594535fd4cef1af1edc897cbdf5a565bcc2ae882
Ekaterina Lysiuk <ekaterina.lysiuk@oxagile.com>
2022-10-14 14:00:40 +0300

    feat: add tests by search type

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("renders App component", () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment(<App />)).toMatchSnapshot();
+   
+  });
+
+  it("renders Search text", () => {    
+    render(<App />);   
+    expect(screen.getByText(/search:/i)).toBeInTheDocument();
+  });
+
+  
+  it("renders buttton", () => {    
+    render(<App />);    
+    // screen.debug();
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/search text/i)).toBeInTheDocument();
+    expect(screen.getByAltText('sun')).toBeInTheDocument();
+    expect(screen.getByDisplayValue('123')).toBeInTheDocument();
+
+  });
 });
